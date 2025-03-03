3d74bfbcac8f6faef591f9022bc9248e35f9cf02
krishnaa212 <krishnadidwania17@gmail.com>
2025-03-04 01:26:02 +0530

    All Doctors Page done

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Appointment = () => {
+
+  const { docId } = useParams();
+  const {doctors} = useContext(AppContext);
+  const [docInfo,setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id===docId)
+    setDocInfo(docInfo);
+  }
+
+  useEffect(()=> {
+    fetchDocInfo();
+  },[doctors,docId])
+
+  console.log(docInfo)
+
   return (
     <div>
       
